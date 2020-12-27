aaa67d0b430c8d0cba0a0504320b30de134f19c4
eldhosempeter07 <eldhosempeter07@gmail.com>
2020-12-27 22:48:36 +0530

    Finishing sigin handle submit

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import { Component } from 'react';
 import { CustomButton } from '../custom-button/custom-button.component';
 import { FormInput } from '../form-input/form-input.component';
 import "./sign-in.style.scss";
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils';
 
 export class SignIn extends Component {
     constructor(props) {
@@ -19,10 +19,17 @@ export class SignIn extends Component {
         this.setState({[name]:value});
     };
     
-    handleSubmit = (event) => {
-        event.preventDefault();
-    
-        this.setState({ email: "", password: "" });
+    handleSubmit = async (event) => {
+        event.preventDefault(); 
+        const{email,password} = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email,password)
+            this.setState({ email: "", password: "" });
+      
+        } catch (error) {
+            console.log(error);      
+        }
+
       };
     
     render() {
@@ -30,7 +37,7 @@ export class SignIn extends Component {
             <div className="sign-in">
                 <h2>I already have an account </h2>
                 <span>Sign in with your email and password</span>
-                <form onClick={this.handleClick}>
+                <form onSubmit={this.handleSubmit}>
                     <FormInput 
                         type="email" 
                         name="email" 
